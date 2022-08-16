837d730e98481f36cbfb0c82b693337ce0c324ba
Daniel <daniel.hammami@yahoo.com>
2022-08-16 16:46:16 +0200

    action_types

--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,12 @@
+import * as actions from "./actionTypes";
+
 // store = []
 
 let lastId = 0;
 
 export default function reducer(state = [], action) {
   switch (action.type) {
-    case "bugAdded":
+    case actions.BUG_ADDED:
       return [
         ...state,
         {
@@ -13,7 +15,7 @@ export default function reducer(state = [], action) {
           resolved: false,
         },
       ];
-    case "bugRemoved":
+    case actions.BUG_REMOVED:
       return state.filter((bug) => bug.id !== action.payload.id);
     default:
       return state;
