d998d0110e8765ffa788645cb98e95a771ceb6ba
Daniel <daniel.hammami@yahoo.com>
2022-09-09 16:23:29 +0200

    creating_slices

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,32 +1,24 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
-
-// Actions types
-
-const BUG_ADDED = "bugAdded";
-const BUG_REMOVED = "bugRemoved";
-const BUG_RESOLVED = "bugResolved";
-
-// Actions Creators
-
-export const bugAdded = createAction("bugAdded");
-export const bugRemoved = createAction("bugRemoved");
-export const bugResolved = createAction("bugResolved");
-
-// Reducer
+import { createSlice } from "@reduxjs/toolkit";
 
 let lastId = 0;
 
-export default createReducer([], {
-  [bugAdded.type]: (bugs, action) => {
-    bugs.push({
-      id: ++lastId,
-      description: action.payload.description,
-      resolved: false,
-    });
-  },
-
-  [bugResolved.type]: (bugs, action) => {
-    const index = bugs.findIndex((bug) => bug.id === action.payload.id);
-    bugs[index].resolved = true;
+const slice = createSlice({
+  name: "bugs",
+  initialState: [],
+  reducers: {
+    bugAdded: (bugs, action) => {
+      bugs.push({
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false,
+      });
+    },
+    bugResolved: (bugs, action) => {
+      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+      bugs[index].resolved = true;
+    },
   },
 });
+
+export const { bugAdded, bugResolved } = slice.actions;
+export default slice.reducer;
