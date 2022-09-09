777db2b8440ded69f4a1dd71c5196e0d99619b89
Daniel <daniel.hammami@yahoo.com>
2022-09-09 17:19:53 +0200

    exercise

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import configureStore from "./store/configureStore";
-import * as actions from "./store/bugs";
+//import * as actions from "./store/bugs";
+import * as actions from "./store/projects";
 
 const store = configureStore();
 
@@ -7,9 +8,9 @@ store.subscribe(() => {
   console.log("Store changed!");
 });
 
-store.dispatch(actions.bugAdded({ description: "Bug A" }));
-store.dispatch(actions.bugAdded({ description: "Bug B" }));
-store.dispatch(actions.bugAdded({ description: "Bug C" }));
-store.dispatch(actions.bugResolved({ id: 1 }));
+store.dispatch(actions.projectAdded({ name: "Project 1" }));
+// store.dispatch(actions.bugAdded({ description: "Bug B" }));
+// store.dispatch(actions.bugAdded({ description: "Bug C" }));
+// store.dispatch(actions.bugResolved({ id: 1 }));
 
 console.log(store.getState());
